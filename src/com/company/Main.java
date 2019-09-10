635949835122c174e179274c4cc7cb70e3809c7a
tkurudi <t.kurudi209@gmail.com>
2019-09-10 14:50:51 +1200

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 1 + 2 = 3
+
+        System.out.println("1 + 2 = " + result);
+
+        boolean isAlien = false;
+
+        if (!isAlien)
+            System.out.println("It is not an alien");
+
+    }
+}
