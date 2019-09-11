35031d4dc0bfb81afafc08bd1709c09a5badebb6
tkurudi <t.kurudi209@gmail.com>
2019-09-11 17:20:15 +1200

    if then

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,22 @@ public class Main {
 
         boolean isAlien = false;
 
-        if (!isAlien)
+        if (!isAlien) {
             System.out.println("It is not an alien");
+            System.out.println("code block");
+        }
+
+        int topScore = 100;
+        if(topScore == 100) {
+            System.out.println("You have the highest score");
+        }
+
+        int secondTopScore = 60;
+
+        if(topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
 
     }
 }
